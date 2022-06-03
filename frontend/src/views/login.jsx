f126eb269bce305daf61197ea88b97ae0c248c48
Yuizz <julianglz@outlook.es>
2022-06-02 23:51:03 -0500

    Improve on login view
    Added validations, error messages, redirection

--- frontend/src/views/login.jsx
+++ frontend/src/views/login.jsx
@@ -1,121 +1,131 @@
 import {
-    Flex, Box, Text,
-    Heading, Link, FormControl,
-    FormLabel, Input, Stack,
-    Checkbox, Button
+  Flex, Box, Text,
+  Heading, Link, FormControl,
+  FormLabel, Input, Stack,
+  Checkbox, Button,
+  Alert, AlertIcon, AlertTitle, AlertDescription,
 } from "@chakra-ui/react"
 import { useState } from 'react'
-import { useFetch } from "../hooks/useFetch"
 import { backendLink } from "../utils/links"
 import { setToken } from '../utils/token'
+import ErrorMessage from '../components/ErrorMessage'
+import { useHistory } from 'react-router-dom'
 
 export function LoginView() {
-    
-    return (
-        <LoginArea/>  
-    )
+  return (
+    <LoginArea/>  
+  )
 }
 
 const LoginArea = () => {
-    return (
-        <Flex minHeight='100vh' width='full' align='center' justifyContent='center'>
-            <Box
-                borderWidth={1}
-                px={4}
-                width='full'
-                maxWidth='450px'
-                borderRadius={10}
-                textAlign='center'
-                boxShadow='lg'
-            >
-                <Box my={8}>
-                    <Heading>Sign In to Your Account</Heading>
-                    <Text>
-                        Or <Link>register</Link>
-                    </Text>
-                </Box>
-                <Box>
-                    <LoginForm/>
-                </Box>
-            </Box>
-        </Flex>
-    )
+  return (
+    <Flex minHeight='100vh' width='full' align='center' justifyContent='center' bg='#048cd0'>
+      <Box
+        bg='white'
+        borderWidth={1}
+        px={4}
+        width='full'
+        maxWidth='450px'
+        borderRadius={10}
+        textAlign='center'
+        boxShadow='lg'
+      >
+        <Box my={8}>
+          <Heading>Sign In to Your Account</Heading>
+          <Text>
+            Or <Link>register</Link>
+          </Text>
+        </Box>
+        <Box>
+          <LoginForm/>
+        </Box>
+      </Box>
+    </Flex>
+  )
 }
 
 const LoginForm = () => {
-    const [email, setEmail] = useState(null)
-    const [password, setPassword] = useState(null)
-    const [fetchStatus, setFetchStatus] = useState(false)
+  const [username, setUsername] = useState(null)
+  const [password, setPassword] = useState(null)
+  const [fetchStatus, setFetchStatus] = useState(false)
+  const [error, setError] = useState(null)
+  const history = useHistory()
 
-    const SignIn = (event) => {
-        event.preventDefault()
-        fetch(backendLink('login'), {
-                method: 'POST',
-                headers: {
-                        'Content-Type': 'application/json',
-                    },
-                body: JSON.stringify({
-                    'username': email,
-                    password
-            })
-        })
-            .then(response => response.json())
-            .then(data => {
-                setToken(data.token)
-            })
-        //TODO make a page with this hook to redirect
-        // const res = useFetch(backendLink('login'), {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //     },
-        //     body: JSON.stringify({
-        //         'username': email,
-        //         password
-        //     })
-        // })
-        // console.log(res)
-        // setFetchStatus(res.isLoading ? true : false)
+  const SignIn = (event) => {
+    setError(null)
 
+    if(!username){
+      setError('Username is required')
     }
+    if(!password){
+      setError('Password is required')
+    }
+    setFetchStatus(true)
+    fetch(backendLink('login'), {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username,
+        password
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+      setFetchStatus(false)
+
+      if(data.token){
+        setToken(data.token)
+        history.push('/profile/'+username+'/')
+      }
+      if(data.non_field_errors){
+        setError(data.non_field_errors)
+      }
+    })
+  }
 
     return (
-        <Box my ={4} textAlign='left'>
-            <form>
-                <FormControl isRequired>
-                    <FormLabel>Email Address</FormLabel>
-                    <Input
-                        type='email'
-                        placeholder='Enter your email address'
-                        onChange={event => setEmail(event.currentTarget.value)}
-                    ></Input>
-                </FormControl>
-                <FormControl isRequired>
-                    <FormLabel>Password</FormLabel>
-                    <Input
-                        type='password'
-                        placeholder='Enter your password'
-                        onChange={event => setPassword(event.currentTarget.value)}
-                        onKeyUp={event => event.keyCode === 13 ? SignIn : 0}
-                    ></Input>
-                </FormControl>
+      <Box my ={4} textAlign='left'>
+        {error && <ErrorMessage message={error}/>}
+        <form>
+          <FormControl isRequired>
+            <FormLabel>Username</FormLabel>
+            <Input
+              type='text'
+              placeholder='Enter your username'
+              onChange={event => setUsername(event.currentTarget.value)}
+              onKeyDown={e=>{ if(e.key === "Enter") SignIn()}}
+            ></Input>
+          </FormControl>
+          <FormControl isRequired>
+            <FormLabel>Password</FormLabel>
+            <Input
+              type='password'
+              placeholder='Enter your password'
+              onChange={event => setPassword(event.currentTarget.value)}
+              onKeyDown={e=>{ if(e.key === "Enter") SignIn()}}
+            ></Input>
+          </FormControl>
 
-                <Stack isInline justifyContent='space-between' mt={4}>
-                    <Box>
-                        <Checkbox>Remember me</Checkbox>
-                    </Box>
-                    <Box>
-                        <Link>Forgot your password?</Link>
-                    </Box>
-                </Stack>
+          <Stack isInline justifyContent='space-between' mt={4}>
+            <Box>
+              <Checkbox>Remember me</Checkbox>
+            </Box>
+            <Box>
+              <Link>Forgot your password?</Link>
+            </Box>
+          </Stack>
 
-                <Button
-                    isLoading={fetchStatus}
-                    loadingText='Iniciando Sesión'
-                    width='full'
-                    mt={4}
-                    onClick={SignIn}>Sign in</Button>
-            </form>
-        </Box>
+          <Button
+            isLoading={fetchStatus}
+            loadingText='Fetching...'
+            disabled={!username || !password}
+            width='full'
+            mt={4}
+            onClick={SignIn}>Sign in
+          </Button>
+        </form>
+      </Box>
     )
 }
