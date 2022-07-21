3d72b2e0f6950a1ac11c9e386d39b50656931107
Yuizz <julianglz@outlook.es>
2022-07-20 20:36:12 -0500

    Format document

--- frontend/src/views/profile/Profile.jsx
+++ frontend/src/views/profile/Profile.jsx
@@ -1,118 +1,143 @@
 import {
-  Flex, Box, Text, Heading,
-  Stack, Button,
-  Avatar, Tabs, TabList, Tab, TabPanels,
-  TabPanel, Tag, StackDivider,
-} from '@chakra-ui/react'
-import { useParams } from 'react-router-dom'
-import { Link } from 'react-router-dom'
-import { DrawerAddProject } from '../../components/drawers'
-import { useFetch } from '../../hooks/useFetch'
-import { links, token, formatDate } from '../../utils'
-import ProjectView from './ProjectView'
+  Flex,
+  Box,
+  Text,
+  Heading,
+  Stack,
+  Button,
+  Avatar,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+  Tag,
+  StackDivider,
+} from "@chakra-ui/react";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { DrawerAddProject } from "../../components/drawers";
+import { useFetch } from "../../hooks/useFetch";
+import { links, token, formatDate } from "../../utils";
+import ProjectView from "./ProjectView";
 import { LoadingElement } from "../../components";
 
 export function ProfileView() {
-  const params = useParams()
+  const params = useParams();
 
-  const res = useFetch(links.backendLink('profile', params.username), {
-    method:'GET',
+  const res = useFetch(links.backendLink("profile", params.username), {
+    method: "GET",
     headers: {
-      'Authorization': 'Token ' + token.getToken()
-    }
-  })
+      Authorization: "Token " + token.getToken(),
+    },
+  });
 
-  if (res.isLoading || !res.response) return <LoadingElement/>
-  if (res.response.errors) return <Heading>{res.response.errors.error}</Heading>
-  const userCanEdit = res.response.links.canEdit
+  if (res.isLoading || !res.response) return <LoadingElement />;
+  if (res.response.errors)
+    return <Heading>{res.response.errors.error}</Heading>;
+  const userCanEdit = res.response.links.canEdit;
 
   return (
-    <Flex h="100%" direction={{ base: 'column', md: 'row', lg: 'row' }}>
+    <Flex h="100%" direction={{ base: "column", md: "row", lg: "row" }}>
       <UserCard
         py={5}
         px={10}
-        maxW={{ md: '382px' }}
-        minW={{md:'344px'}}
-        w={{ base: '100%', md: '50%', lg: '35%' }}
-        height='full'
-        display={{ base: 'flex', md: 'block', lg: 'block' }}
+        maxW={{ md: "382px" }}
+        minW={{ md: "344px" }}
+        w={{ base: "100%", md: "50%", lg: "35%" }}
+        height="full"
+        display={{ base: "flex", md: "block", lg: "block" }}
         userData={res.response.data}
-        align='center'
+        align="center"
         canEdit={userCanEdit}
       />
       <Container
-        mt='1%'
-        width='100%'
-        height='100vh'
-        projects = {res.response.data.projects}
+        mt="1%"
+        width="100%"
+        height="100vh"
+        projects={res.response.data.projects}
         params={params}
         reFetchProjects={res.reFetch}
         canEdit={userCanEdit}
       />
     </Flex>
-  )
+  );
 }
 
-const UserCard = ({userData, canEdit, ...props }) => {
-  const firstName = userData.first_name
-  const lastName = userData.last_name
-  const username = userData.username
-  const avatarUrl = userData.avatar_url
+const UserCard = ({ userData, canEdit, ...props }) => {
+  const firstName = userData.first_name;
+  const lastName = userData.last_name;
+  const username = userData.username;
+  const avatarUrl = userData.avatar_url;
 
   return (
     <Box {...props}>
       <Box my={5}>
         <Avatar
           src={avatarUrl}
-          name={'User Avatar'}
-          size={{base:'xl', md:'2xl'}} />
+          name={"User Avatar"}
+          size={{ base: "xl", md: "2xl" }}
+        />
       </Box>
-      <Box
-        width='full'
-        ml={{base:10, md:0}}
-        verticalAlign={'middle'}
-      >
-        <Heading
-          align='left'
-          size={'md'}
-        >{firstName} {lastName}</Heading>
-        <Text fontSize={{base: 'md', lg: 'xl'}}
-              fontWeight='thin'
-              color='gray'
-              align='left'
-        >{username}</Text>
+      <Box width="full" ml={{ base: 10, md: 0 }} verticalAlign={"middle"}>
+        <Heading align="left" size={"md"}>
+          {firstName} {lastName}
+        </Heading>
+        <Text
+          fontSize={{ base: "md", lg: "xl" }}
+          fontWeight="thin"
+          color="gray"
+          align="left"
+        >
+          {username}
+        </Text>
 
-        {canEdit ?
+        {canEdit ? (
           <Button
             mt={4}
-            width={{base: 'max-content', md: 'full', lg: 'full'}}
-            variant={'outline'}
+            width={{ base: "max-content", md: "full", lg: "full" }}
+            variant={"outline"}
           >
             Cambiar datos
           </Button>
-          : ''}
+        ) : (
+          ""
+        )}
       </Box>
     </Box>
-  )
-}
+  );
+};
 
-const Container = ({projects, params, reFetchProjects, canEdit, ...props}) => {
+const Container = ({
+  projects,
+  params,
+  reFetchProjects,
+  canEdit,
+  ...props
+}) => {
   return (
     <Box {...props}>
-      <Tabs
-        colorScheme={'blue'}
-      >
+      <Tabs colorScheme={"blue"}>
         <TabList>
-          <Tab id={'projects'}>Proyectos</Tab>
-          <Tab id={'information'}>Información</Tab>
+          <Tab id={"projects"}>Proyectos</Tab>
+          <Tab id={"information"}>Información</Tab>
         </TabList>
 
         <TabPanels>
           <TabPanel>
-
-            {!params.project ? <Projects projects={projects} reFetchProjects={reFetchProjects} canEdit={canEdit}/>
-              : <ProjectView projects={projects} reFetchProjects={reFetchProjects} canEdit={canEdit}/>}
-
+            {!params.project ? (
+              <Projects
+                projects={projects}
+                reFetchProjects={reFetchProjects}
+                canEdit={canEdit}
+              />
+            ) : (
+              <ProjectView
+                projects={projects}
+                reFetchProjects={reFetchProjects}
+                canEdit={canEdit}
+              />
+            )}
           </TabPanel>
           <TabPanel>
             <p>Esta es la pestaña de información</p>
@@ -120,54 +145,38 @@ const Container = ({projects, params, reFetchProjects, canEdit, ...props}) => {
         </TabPanels>
       </Tabs>
     </Box>
-  )
-}
+  );
+};
 
-function Projects({projects, reFetchProjects, canEdit, ...props}) {
-  return(
+function Projects({ projects, reFetchProjects, canEdit, ...props }) {
+  return (
     <>
-      <Stack
-        isInline
-        p={3}
-      >
-        {canEdit ?
-          <DrawerAddProject reFetch={reFetchProjects}/>
-        : ''}
+      <Stack isInline p={3}>
+        {canEdit ? <DrawerAddProject reFetch={reFetchProjects} /> : ""}
       </Stack>
-      <Box
-        borderWidth={1}
-        borderRadius={10}
-        p={5}
-      >
-        <Stack
-          divider={<StackDivider borderColor="gray.200" />}
-        >
-          {projects.map((project, index)=> {
-            const lastUpdate = formatDate(project.updated_at)
+      <Box borderWidth={1} borderRadius={10} p={5}>
+        <Stack divider={<StackDivider borderColor="gray.200" />}>
+          {projects.map((project, index) => {
+            const lastUpdate = formatDate(project.updated_at);
 
             return (
-              <Flex
-                key={project.id}
-                height={12}
-                width={'full'}
-                mb={5}
-              >
+              <Flex key={project.id} height={12} width={"full"} mb={5}>
                 <Stack>
                   <Stack isInline>
-                            <Heading fontSize="xl">
-                                <Link to={`${index+1}`} >{project.name}</Link>
-                            </Heading>
-                    <Tag colorScheme={project.status ? 'green' : 'red'} ml={1}>
-                      {project.status ? 'Abierto' : 'Cerrado'}
+                    <Heading fontSize="xl">
+                      <Link to={`${project.id}`}>{project.name}</Link>
+                    </Heading>
+                    <Tag colorScheme={project.status ? "green" : "red"} ml={1}>
+                      {project.status ? "Abierto" : "Cerrado"}
                     </Tag>
                   </Stack>
                   <Text fontSize="sm">Ultima actualización {lastUpdate}</Text>
                 </Stack>
               </Flex>
-            )
+            );
           })}
         </Stack>
       </Box>
     </>
-  )
+  );
 }
--- frontend/src/views/profile/ProjectView.jsx
+++ frontend/src/views/profile/ProjectView.jsx
@@ -1,45 +1,69 @@
 import {
-  Box, Button, ButtonGroup, Center, createStandaloneToast, Flex,
-  Heading, Stack, StackDivider,
-  Tag, Text, Tooltip
+  Box,
+  Button,
+  ButtonGroup,
+  Center,
+  createStandaloneToast,
+  Flex,
+  Heading,
+  Stack,
+  StackDivider,
+  Tag,
+  Text,
+  Tooltip,
 } from "@chakra-ui/react";
-import { useParams } from 'react-router-dom'
+import { useParams } from "react-router-dom";
 import { IssueDrawer, DrawerAddIssue } from "../../components/drawers";
 import { ArrowBackIcon, ArrowForwardIcon, Icon } from "@chakra-ui/icons";
 import { useFetch } from "../../hooks/useFetch";
 import { LoadingElement } from "../../components";
 import { links, token, formatDate, issueStatus, labelColor } from "../../utils";
 import React, { useState } from "react";
-import {ErrorMessage, DeleteAlertDialog} from "../../components/";
+import { ErrorMessage, DeleteAlertDialog } from "../../components/";
 
-export default function ProjectView({projects, reFetchProjects,canEdit, ...props}){
-  const params = useParams()
-  const project = projects[params.project-1]
-  const [query, setQuery] = useState(links.backendLink('issues', `?page=${1}&project=${project.id}`))
+export default function ProjectView({
+  projects,
+  reFetchProjects,
+  canEdit,
+  ...props
+}) {
+  const params = useParams();
+  const project = projects[params.project - 1];
+  const [query, setQuery] = useState(
+    links.backendLink("issues", `?page=${1}&project=${project.id}`)
+  );
 
-  const res = useFetch(query, {
-    method:'GET',
-    headers: {
-      'Authorization': 'Token ' + token.getToken()
-    }
-  }, [query])
+  const res = useFetch(
+    query,
+    {
+      method: "GET",
+      headers: {
+        Authorization: "Token " + token.getToken(),
+      },
+    },
+    [query]
+  );
 
-  if (res.isLoading || !res.response) return <LoadingElement/>
-  if (res.response.errors) return <Center height={'25vh'}><ErrorMessage message={res.response.errors.error}/></Center>
+  if (res.isLoading || !res.response) return <LoadingElement />;
+  if (res.response.errors)
+    return (
+      <Center height={"25vh"}>
+        <ErrorMessage message={res.response.errors.error} />
+      </Center>
+    );
 
-
-  const issues = res.response.results
+  const issues = res.response.results;
 
   const smoothScrollToTop = () => {
-    const h = window.scrollY
+    const h = window.scrollY;
 
-    if(h>0){
-      setTimeout(()=>{
-        window.scrollTo(0, h-10)
-        smoothScrollToTop()
-      },3)
+    if (h > 0) {
+      setTimeout(() => {
+        window.scrollTo(0, h - 10);
+        smoothScrollToTop();
+      }, 3);
     }
-  }
+  };
   //
   // const onDelete = () => {
   //   fetch(project.url, {
@@ -73,110 +97,113 @@ export default function ProjectView({projects, reFetchProjects,canEdit, ...props
   //   })
   // }
 
-  function handlePrevious(){
-    setQuery(res.response.previous)
-    res.reFetch()
-    smoothScrollToTop()
+  function handlePrevious() {
+    setQuery(res.response.previous);
+    res.reFetch();
+    smoothScrollToTop();
   }
 
-  function handleNext(){
-    setQuery(res.response.next)
-    res.reFetch()
-    smoothScrollToTop()
+  function handleNext() {
+    setQuery(res.response.next);
+    res.reFetch();
+    smoothScrollToTop();
   }
 
-  return(
-    <Box
-      width={'full'}
-      height={'full'}
-      {...props}
-    >
-      <Stack isInline p={3} justifyContent={'space-between'}>
-        <Heading noOfLines={1}>{project ? project.name : 'NoName'}</Heading>
+  return (
+    <Box width={"full"} height={"full"} {...props}>
+      <Stack isInline p={3} justifyContent={"space-between"}>
+        <Heading noOfLines={1}>{project ? project.name : "NoName"}</Heading>
 
-        {canEdit ?
-          <ButtonGroup size={'md'}>
+        {canEdit ? (
+          <ButtonGroup size={"md"}>
             <DeleteAlertDialog
-              title={'¿Borrar proyecto?'}
-              message={'¿Estas seguro que quieres borrar el proyecto? Esta acción no se puede deshacer.'}
+              title={"¿Borrar proyecto?"}
+              message={
+                "¿Estas seguro que quieres borrar el proyecto? Esta acción no se puede deshacer."
+              }
               isDisabled
             />
-          <DrawerAddIssue projectId={project ? project.id : ''} reFetch={res.reFetch}/>
-        </ButtonGroup>
-          : ''
-        }
+            <DrawerAddIssue
+              projectId={project ? project.id : ""}
+              reFetch={res.reFetch}
+            />
+          </ButtonGroup>
+        ) : (
+          ""
+        )}
       </Stack>
 
-      <Box
-        borderWidth={1}
-        borderRadius={10}
-        p={5}
-      >
-
-        <Stack
-          divider={<StackDivider borderColor="gray.200" />} >
-          {!res.isLoading ?
-            issues.map(issue=>issueCard(issue, res.reFetch, canEdit))
-            : <LoadingElement/>
-          }
+      <Box borderWidth={1} borderRadius={10} p={5}>
+        <Stack divider={<StackDivider borderColor="gray.200" />}>
+          {!res.isLoading ? (
+            issues.map((issue) => issueCard(issue, res.reFetch, canEdit))
+          ) : (
+            <LoadingElement />
+          )}
         </Stack>
-
       </Box>
-      <Stack isInline mt={'10px'} justifyContent={'center'} width={'full'}>
-        <ButtonGroup size={'sm'} isAttached variant={'outline'}>
-            <Button aria-label={'Previous page'}
-                    isDisabled={!res.response.previous}
-                    colorScheme={'blue'}
-                    onClick={handlePrevious}
-                    leftIcon={<ArrowBackIcon/>}>
-              Anterior
-            </Button>
+      <Stack isInline mt={"10px"} justifyContent={"center"} width={"full"}>
+        <ButtonGroup size={"sm"} isAttached variant={"outline"}>
+          <Button
+            aria-label={"Previous page"}
+            isDisabled={!res.response.previous}
+            colorScheme={"blue"}
+            onClick={handlePrevious}
+            leftIcon={<ArrowBackIcon />}
+          >
+            Anterior
+          </Button>
 
-            <Button aria-label={'Next page'}
-                    isDisabled={!res.response.next}
-                    colorScheme={'blue'}
-                    onClick={handleNext}
-                    rightIcon={<ArrowForwardIcon/>} >
-              Siguiente
-            </Button>
+          <Button
+            aria-label={"Next page"}
+            isDisabled={!res.response.next}
+            colorScheme={"blue"}
+            onClick={handleNext}
+            rightIcon={<ArrowForwardIcon />}
+          >
+            Siguiente
+          </Button>
         </ButtonGroup>
-
       </Stack>
     </Box>
-  )
+  );
 }
 
 const issueCard = (issue, reFetch, canEdit) => {
-  const lastUpdate = formatDate(issue.updated_at)
-  const status = issueStatus[issue.status]
+  const lastUpdate = formatDate(issue.updated_at);
+  const status = issueStatus[issue.status];
 
-  return(
-    <Flex
-      key={issue.id}
-      height={'auto'}
-      width={'full'}
-    >
+  return (
+    <Flex key={issue.id} height={"auto"} width={"full"}>
       <Stack isInline>
         <Stack>
           <Stack isInline>
-            {issue.label ? issue.label.name : ''}
+            {issue.label ? issue.label.name : ""}
             <Stack>
               <Stack isInline>
-                <IssueDrawer issue={issue} reFetch={reFetch} canEdit={canEdit}/>
-                <Tag colorScheme={ status.color }>
-                  <Icon as={status.icon} mx="3px"/> {status.name}
+                <IssueDrawer
+                  issue={issue}
+                  reFetch={reFetch}
+                  canEdit={canEdit}
+                />
+                <Tag colorScheme={status.color}>
+                  <Icon as={status.icon} mx="3px" /> {status.name}
                 </Tag>
                 <Tag
-                  colorScheme={ issue.label ? labelColor[issue.label.name] : '' }
-                  borderRadius={ 20 }
-                  width={ 'max-content' }
-                >{ issue.label ? issue.label.name : '' }</Tag>
+                  colorScheme={issue.label ? labelColor[issue.label.name] : ""}
+                  borderRadius={20}
+                  width={"max-content"}
+                >
+                  {issue.label ? issue.label.name : ""}
+                </Tag>
               </Stack>
             </Stack>
           </Stack>
-          <Text fontSize={ 'xs' } textColor={ 'gray.500' }>Ultima actualización { lastUpdate }</Text>
+          <Text fontSize={"xs"} textColor={"gray.500"}>
+            Ultima actualización {lastUpdate}
+          </Text>
         </Stack>
       </Stack>
     </Flex>
-  )
-}
+  );
+};
